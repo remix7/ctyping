c3497290caa0cf58202bbf7ee7f0b7f6ed297af0
remix7 <remix7@live.cn>
2016-11-06 00:03:20 +0800

    完善功能  添加注释

--- src/main/java/top/cynara/ctyping/service/ExamService.java
+++ src/main/java/top/cynara/ctyping/service/ExamService.java
@@ -3,18 +3,26 @@ package top.cynara.ctyping.service;
 import java.util.List;
 
 import top.cynara.ctyping.entitiy.Exam;
+
 /**
- * @ClassName ExamService 
+ * @ClassName ExamService
  * @Description 测试操作
- * @author Cynara-remix http://cynara.top
- * E-mail remix7@live.cn 
- * @date 2016年10月21日 下午5:58:34 
+ * @author Cynara-remix http://cynara.top E-mail remix7@live.cn
+ * @date 2016年10月21日 下午5:58:34
  * @version V1.0
  */
 public interface ExamService {
 	void insert(Exam exam);
+
 	void delete(Integer id);
+
 	void update(Exam exam);
+
 	Exam findById(Integer id);
+
 	List<Exam> findAll();
+
+	List<Exam> findAllTest();
+
+	List<Exam> findAllExam();
 }
