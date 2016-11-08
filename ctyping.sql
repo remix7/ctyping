864474c41b2d1c048cb26a12a4367d30aacc1e31
remix7 <remix7@live.cn>
2016-11-08 15:08:08 +0800

    结构更新

--- ctyping.sql
+++ ctyping.sql
@@ -26,7 +26,9 @@ CREATE TABLE `ctyping_exam_recodes` (
   `user_id` int(11) default NULL,
   `content` text,
   `useTime` varchar(5) default NULL,
-  `updateTime` datetime default NULL,
+  `score` varchar(100) default NULL,
+  `accuracy` varchar(100) default NULL,
+  `updateTime` varchar(20) default NULL,
   `state` varchar(1) default NULL,
   `remarks` varchar(100) default NULL,
   PRIMARY KEY  (`id`),
@@ -34,7 +36,7 @@ CREATE TABLE `ctyping_exam_recodes` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `ctyping_exam_recodes_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `ctyping_exams` (`id`),
   CONSTRAINT `ctyping_exam_recodes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `ctyping_users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=162 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `ctyping_exams` */
 
@@ -43,16 +45,30 @@ DROP TABLE IF EXISTS `ctyping_exams`;
 CREATE TABLE `ctyping_exams` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(100) default NULL,
-  `beginTime` datetime default NULL,
-  `endTime` datetime default NULL,
+  `beginTime` varchar(20) default NULL,
+  `endTime` varchar(20) default NULL,
   `question_id` int(11) default NULL,
-  `createTime` datetime default NULL,
+  `createTime` varchar(20) default NULL,
   `state` varchar(1) default NULL,
   `remarks` varchar(100) default NULL,
   PRIMARY KEY  (`id`),
   KEY `question_id` (`question_id`),
   CONSTRAINT `ctyping_exams_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `ctyping_questions` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ctyping_permission` */
+
+DROP TABLE IF EXISTS `ctyping_permission`;
+
+CREATE TABLE `ctyping_permission` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(20) default NULL,
+  `type` varchar(20) default NULL,
+  `url` varchar(100) default NULL,
+  `percode` varchar(20) default NULL,
+  `avaliable` varchar(20) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `ctyping_questions` */
 
@@ -64,11 +80,37 @@ CREATE TABLE `ctyping_questions` (
   `content` text,
   `degree` varchar(10) default NULL,
   `contentLength` varchar(5) default NULL,
-  `createTime` datetime default NULL,
+  `createTime` varchar(20) default NULL,
   `state` varchar(1) default NULL,
   `remarks` varchar(100) default NULL,
   PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ctyping_role` */
+
+DROP TABLE IF EXISTS `ctyping_role`;
+
+CREATE TABLE `ctyping_role` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(20) default NULL,
+  `avaliable` varchar(1) default NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ctyping_role_permission` */
+
+DROP TABLE IF EXISTS `ctyping_role_permission`;
+
+CREATE TABLE `ctyping_role_permission` (
+  `id` int(11) NOT NULL auto_increment,
+  `role_id` int(11) default NULL,
+  `permission_id` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `permission_id` (`permission_id`),
+  CONSTRAINT `ctyping_role_permission_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `ctyping_role` (`id`),
+  CONSTRAINT `ctyping_role_permission_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `ctyping_permission` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `ctyping_scores` */
 
@@ -80,10 +122,10 @@ CREATE TABLE `ctyping_scores` (
   `user_id` int(11) default NULL,
   `content` text,
   `useTime` varchar(5) default NULL,
-  `score` varchar(5) default NULL,
-  `accuracy` varchar(5) default NULL,
+  `score` varchar(20) default NULL,
+  `accuracy` varchar(20) default NULL,
   `ranking` int(11) default NULL,
-  `createTime` datetime default NULL,
+  `createTime` varchar(20) default NULL,
   `state` varchar(1) default NULL,
   `remarks` varchar(100) default NULL,
   PRIMARY KEY  (`id`),
@@ -91,6 +133,21 @@ CREATE TABLE `ctyping_scores` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `ctyping_scores_ibfk_1` FOREIGN KEY (`exam_id`) REFERENCES `ctyping_exams` (`id`),
   CONSTRAINT `ctyping_scores_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `ctyping_users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ctyping_user_role` */
+
+DROP TABLE IF EXISTS `ctyping_user_role`;
+
+CREATE TABLE `ctyping_user_role` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) default NULL,
+  `role_id` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `ctyping_user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `ctyping_users` (`id`),
+  CONSTRAINT `ctyping_user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `ctyping_role` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `ctyping_users` */
@@ -105,11 +162,11 @@ CREATE TABLE `ctyping_users` (
   `stuNum` varchar(20) default NULL,
   `phoneNum` varchar(15) default NULL,
   `email` varchar(30) default NULL,
-  `createTime` datetime default NULL,
+  `createTime` varchar(20) default NULL,
   `state` varchar(1) default NULL,
   `remarks` varchar(100) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
